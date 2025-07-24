da0520dfed989a68f75c2ef8be78ae36c4eb91a4
sukarthy11 <sukarthym@gmail.com>
2025-07-24 15:36:23 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,22 @@
+//let number=[1,2,"three",true];
+
+//let names = new Array("john","james");
+
+//console.log(number[2]);
+//console.log(names,length);
+//filter()
+
+//let result=number.filter(result => number[3]);
+//console.log(result);
+//slice = delete the element based on index value 
+//let r=[1,2,3,4,5];
+ //r.splice(1,2,"haii");
+//console.log(r)
+
+//object
+let details ={
+    names:"vave",
+    totalmark:"95"
+}
+console.log(details.totalmark)
+console.log(details["names"]);
